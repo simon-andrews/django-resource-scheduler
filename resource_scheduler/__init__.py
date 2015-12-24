6032c4d7cfd6266a6dbd6ddddb80aaf7f8f73043
Simon Andrews <simon.andrews212@gmail.com>
2015-12-24 12:12:03 -0500

    Set up Django Admin and make Resource model

---
+++ resource_scheduler/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'resource_scheduler.apps.ResourceSchedulerConfig'
--- resource_scheduler/apps.py
+++ resource_scheduler/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ResourceSchedulerConfig(AppConfig):
     name = 'resource_scheduler'
+    verbose_name = 'Resource Scheduler'
