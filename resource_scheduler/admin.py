6379796cb7c3a16c5f0a24cf1e4e755ed3bbb314
Simon Andrews <simon.andrews212@gmail.com>
2015-12-24 14:16:23 -0500

    Update Reservation model with new fields

--- resource_scheduler/admin.py
+++ resource_scheduler/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Resource
+from .models import Resource, Reservation
+
+class ReservationAdmin(admin.ModelAdmin):
+	list_display = (
+		'user',
+		'resource',
+		'priority',
+		'reservation_start',
+		'reservation_end',
+		'due_date',
+		'message',
+	)
 
 admin.site.register(Resource)
+admin.site.register(Reservation, ReservationAdmin)
--- resource_scheduler/models.py
+++ resource_scheduler/models.py
@@ -1,7 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+priority_choices = (
+	('l', 'Low'),
+	('n', 'Normal'),
+	('h', 'High'),
+	('c', 'Critical')
+)
 
 class Resource(models.Model):
 	name = models.CharField(max_length=100)
-	
 	def __str__(self):
 		return self.name
+
+class Reservation(models.Model):
+	user = models.ForeignKey(User)
+	resource = models.ForeignKey(Resource)
+	priority = models.CharField(max_length=1, choices=priority_choices)
+	reservation_start = models.DateTimeField()
+	reservation_end = models.DateTimeField()
+	due_date = models.DateTimeField(blank=True, null=True)
+	message = models.TextField(blank=True, null=True)
+	def __str__(self):
+		return "{r} ({u}/{p})".format(r=self.resource,
+											u=self.user,
+											p=self.priority)
