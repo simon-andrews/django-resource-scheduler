98f41df76970e29c22b4a74c02af3f72b9f680cf
Simon Andrews <simon.andrews212@gmail.com>
2015-12-26 09:11:09 -0500

    Add view for listing resources

--- resource_scheduler/views.py
+++ resource_scheduler/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from .models import Resource
 
 def index(request):
     return render_to_response("index.html")
+
+def allresources(request):
+	mdict = {
+		"resources": Resource.objects.all()
+	}
+	return render_to_response("resources_main.html", mdict)
